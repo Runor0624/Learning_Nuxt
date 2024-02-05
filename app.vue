6f5236a8ff820ee9cee00efbb2c686fd089ff3b4
한상일 <87845749+Runor0624@users.noreply.github.com>
2024-02-05 21:52:53 +0900

    데이터 fetch 추가

--- app.vue
+++ app.vue
@@ -1,7 +1,22 @@
+<script setup lang="ts"> {/* 스크립트는 타입스크립트를 사용할꺼다. */}
+interface Post {
+  userId: number
+  id: number 
+  title: string
+  body: string 
+}
+
+const { data } = await useFetch<Post[]>('https://jsonplaceholder.typicode.com/posts') 
+</script>
+
 <template>
   <div>
-    <h1 className="text-center">Vue Test</h1>
-    <button className="btn btn-primary">Test</button>
-    <NuxtWelcome />
+    <div v-for="post in data" :key="post.id"> <!-- v-for : React의 map 처럼 반복하여 렌더링하는 역할을 담당함.-->
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
   </div>
 </template>
+
+
+
